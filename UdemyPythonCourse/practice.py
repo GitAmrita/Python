25fa4a6f5edea06ec4eb4e1814851a337ec27e86
GitAmrita <careers.amrita@gmail.com>
2017-09-03 09:34:42 -0700

    Commit

--- UdemyPythonCourse/practice.py
+++ UdemyPythonCourse/practice.py
@@ -1,21 +1,12 @@
-class A(object):     # deriving from 'object' declares A as a 'new-style-class'
-    def __init__(self):
-        self.k = 1
+import re
 
 
-    def foo(self):
-        print "foo"
-
-class B(A):
-    def __init__(self):
-        self.k1 = 11
-
-
-    def foo(self):
-        super(B, self).foo()   # calls 'A.foo()'
-
-myB = B()
-myB.foo()
+pattern = r"eggs(bacon)*"
+string = "test"
+if re.match(pattern, string):
+    print 1
+else:
+    print 0
 
 
 
